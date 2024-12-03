973e1dd65b455cb737b58a9f2912e3c91a90f4b3
liyi <swl1911@iCloud.com>
2024-12-03 21:56:51 +0800

    第三次提交 thirdly commit

--- src/main/java/com/ry/HelloWord.java
+++ src/main/java/com/ry/HelloWord.java
@@ -4,5 +4,6 @@ public class HelloWord {
     public static void main(String[] args){
         System.out.println("你好 Git");
         System.out.println("你好 Git");
+        System.out.println("你好 Git333");
     }
 }
