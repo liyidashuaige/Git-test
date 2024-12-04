d22bd93efc1b67730dd901885014ae139baf4085
liyi <swl1911@iCloud.com>
2024-12-04 11:41:27 +0800

    hot-fix second commit(冲突合并)

--- src/main/java/com/ry/HelloWord.java
+++ src/main/java/com/ry/HelloWord.java
@@ -6,5 +6,6 @@ public class HelloWord {
         System.out.println("你好 Git");
         System.out.println("你好 Git333");
         System.out.println("hot-fix分支添加功能");
+        System.out.println("hot-fix 合并冲突代码");
     }
 }
