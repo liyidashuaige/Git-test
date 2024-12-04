afa5db1f98e2b11ca8fc68e1b619accc351be618
liyi <swl1911@iCloud.com>
2024-12-04 11:32:54 +0800

    hot-fix分支添加功能

--- src/main/java/com/ry/HelloWord.java
+++ src/main/java/com/ry/HelloWord.java
@@ -5,5 +5,6 @@ public class HelloWord {
         System.out.println("你好 Git");
         System.out.println("你好 Git");
         System.out.println("你好 Git333");
+        System.out.println("hot-fix分支添加功能");
     }
 }
