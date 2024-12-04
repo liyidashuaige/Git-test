d38aac15e596dcc128a002b47b7a47ecaf754c2d
liyi <swl1911@iCloud.com>
2024-12-04 11:43:41 +0800

    master (冲突合并)

--- src/main/java/com/ry/HelloWord.java
+++ src/main/java/com/ry/HelloWord.java
@@ -6,5 +6,6 @@ public class HelloWord {
         System.out.println("你好 Git");
         System.out.println("你好 Git333");
         System.out.println("hot-fix分支添加功能");
+        System.out.println("master 测试合并冲突...");
     }
 }
